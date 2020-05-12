a0adeb7a514b3e2f865bed8713b00fd28d3959a7
MihaiBalosu <balosu_mihai@yahoo.com>
2020-05-12 19:08:33 +0300

    Tests

---
+++ TBPB-Shop/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TBPB_Shop.ApplicationLogic.Models;
+
+namespace UnitTests1
+{
+    [TestClass]
+    public class CategoryTest
+    {
+        [TestMethod]
+        public void CreateCategory_Return_NewCategory()
+        {
+            string categoryName = "Monitors";
+            string categoryDescription = "Output device";
+
+            var category = Category.Create(categoryName, categoryDescription);
+
+            Assert.AreEqual(categoryName, category.Name);
+            Assert.AreEqual(categoryDescription, category.Description);
+        }
+
+        [TestMethod]
+        public void CreateUpdate_Return_UpdatedCategory()
+        {
+            Category categoryObject = Category.Create("Mobilier", "Scaun");
+            var initialCategoryName = categoryObject.Name;
+            var initialCategoryDescription = categoryObject.Description;
+
+            string newCategoryName = "Ustensile";
+            string newCategoryDescriptio = "Cutit";
+
+            categoryObject.CreateForUpdate(newCategoryName, newCategoryDescriptio);
+
+            Assert.AreNotEqual(categoryObject.Name, initialCategoryName);
+            Assert.AreNotEqual(categoryObject.Description, initialCategoryDescription);
+        }
+
+    }
+}
